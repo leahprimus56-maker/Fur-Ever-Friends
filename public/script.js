767a7526d12972fec4f979c8cfc30652d5c6e851
surenkatwaroo1-beep <surenkatwaroo1@gmail.com>
2026-04-10 20:33:13 -0400

    homepage edited

--- public/script.js
+++ public/script.js
@@ -1,30 +1,16 @@
-let pets = [
-  {name: "Max", breed: "Golden Retriever"},
-  {name: "Bella", breed: "Bulldog"},
-  {name: "Rocky", breed: "Poodle"},
-  {name: "Luna", breed: "Beagle"},
-  {name: "Milo", breed: "Terrier"}
-];
+let pets = ["Pet1", "Pet2", "Pet3", "Pet4", "Pet5"];
 
 let index = 0;
 
-/* SHOW PETS */
 function showPets() {
   let container = document.getElementById("pet-container");
   container.innerHTML = "";
 
   for (let i = index; i < index + 3 && i < pets.length; i++) {
-    let pet = pets[i];
-
-    container.innerHTML +=
-      "<div class='pet'>" +
-      "<h4>" + pet.name + "</h4>" +
-      "<p>" + pet.breed + "</p>" +
-      "</div>";
+    container.innerHTML += "<div class='pet'>" + pets[i] + "</div>";
   }
 }
 
-/* NEXT */
 function next() {
   if (index + 3 < pets.length) {
     index += 3;
@@ -32,7 +18,6 @@ function next() {
   }
 }
 
-/* PREVIOUS */
 function prev() {
   if (index - 3 >= 0) {
     index -= 3;
@@ -40,21 +25,18 @@ function prev() {
   }
 }
 
-/* STORIES */
 function toggleStory(el) {
-  let p = el.querySelector("p");
-  p.classList.toggle("hidden");
+  let hidden = el.querySelector(".hidden");
+  hidden.style.display =
+    hidden.style.display === "block" ? "none" : "block";
 }
 
-/* SCROLL */
 function scrollToPets() {
   document.getElementById("featured").scrollIntoView();
 }
 
-/* SIGNUP */
 function signup() {
-  alert("Signup coming soon!");
+  alert("Signup");
 }
 
-/* START */
 showPets();
