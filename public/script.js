4cdaaf4efe235b277a14c614a809ddec96b9730b
surenkatwaroo1-beep <surenkatwaroo1@gmail.com>
2026-04-11 22:57:41 -0400

    home update

--- public/script.js
+++ public/script.js
@@ -1,42 +1,30 @@
-let pets = ["Pet1", "Pet2", "Pet3", "Pet4", "Pet5"];
-
+let pets = [];
 let index = 0;
 
+/* LOAD FROM API */
+async function loadPets() {
+  try {
+    let response = await fetch("https://freetestapi.com/api/v1/dogs");
+    pets = await response.json();
+    showPets();
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+/* DISPLAY */
 function showPets() {
   let container = document.getElementById("pet-container");
   container.innerHTML = "";
 
   for (let i = index; i < index + 3 && i < pets.length; i++) {
-    container.innerHTML += "<div class='pet'>" + pets[i] + "</div>";
+    let pet = pets[i];
+
+    container.innerHTML += `
+      <div class="pet">
+        <img src="https://placedog.net/200/150?random=${i}">
+        <p>${pet.name}</p>
+      </div>
+    `;
   }
 }
-
-function next() {
-  if (index + 3 < pets.length) {
-    index += 3;
-    showPets();
-  }
-}
-
-function prev() {
-  if (index - 3 >= 0) {
-    index -= 3;
-    showPets();
-  }
-}
-
-function toggleStory(el) {
-  let hidden = el.querySelector(".hidden");
-  hidden.style.display =
-    hidden.style.display === "block" ? "none" : "block";
-}
-
-function scrollToPets() {
-  document.getElementById("featured").scrollIntoView();
-}
-
-function signup() {
-  alert("Signup");
-}
-
-showPets();
